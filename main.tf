49f6e4c255a5d95c5b786445cf2f921cb09d1cbe
Mark Kour <mark.kour@icloud.com>
2023-12-04 20:08:43 +0100

    Fix version syntax

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=1.1.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=> 3.0.2"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
   cloud {
